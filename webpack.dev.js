6f806eb760166574b5c8510304ad53a6f218aa40
dmitrybndar <dmitry.bndar@gmail.com>
2018-04-23 23:52:00 +0300

    Fixed env in webpack configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,17 @@
 const merge = require('webpack-merge');
+const DotEnv = require('dotenv');
 const common = require('./webpack.common.js');
 const path = require('path');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' });
+  DotEnv.config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' });
+  DotEnv.config({ path: '.env.development' });
 }
 
-module.exports = merge(common, {
+module.exports = merge(common(process.env), {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
